304455adb1b0426b10e6bf757f4a03f9c3b7d0d4
BrunoCarOliveira <indiadreamloja@gmail.com>
2024-04-29 21:30:19 -0300

    Projeto Previsão do Tempo

---
+++ script.js
@@ -0,0 +1,25 @@
+const key = "f9c21571e30d9be2c20a02d21ad25f1d"
+
+
+function colocarDadosNaTela(dados){
+document.querySelector(".city").innerHTML = "Tempo em " + dados.name
+document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+document.querySelector(".umidade").innerHTML = (dados.humidity) + "%"
+document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+
+}
+
+async function buscarCidade(cidade){
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+    colocarDadosNaTela(dados)
+}
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-city").value
+
+    buscarCidade(cidade)
+}
